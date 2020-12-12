8548c11e2b27e1a34eafbf9a085aaab749819126
breakfastitem <andrew.ehrman@gmail.com>
2020-12-12 03:14:19 -0500

    Added content to questions and called prompt with promise statements

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
+/**
+ * Import Dependencies
+ */
 const fs = require('fs');
 const inquirer= require('inquirer');
 
 const mkDownGenerator = require('./utils/generateMarkdown');
 
+//Define File Name
+
+const fileName ="MReadME.txt";
+
 
 // array of questions for user
 const questions = [
 {
+    type: "input",
+    message: "What is your Github Username?",
+    name: "userName"
 
 },
 {
-
+    
+    type: "input",
+    message: "What is your email Address?",
+    name: "email"
 },
 {
-
+    
+    type: "input",
+    message: "What is your project name?",
+    name: "title"
 }
 ];
 
@@ -32,7 +48,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((response)=>writeToFile(fileName,response));
 }
 
 // function call to initialize program
