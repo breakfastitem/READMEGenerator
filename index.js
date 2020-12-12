4424fb01f1769d3953022c11134b1efaeef6d8e1
breakfastitem <andrew.ehrman@gmail.com>
2020-12-12 01:41:38 -0500

    Installed and imported all file dependencies

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const fs = require('fs');
+const inquirer= require('inquirer');
+
+const mkDownGenerator = require('generateMarkdown');
+
+
 // array of questions for user
 const questions = [
+{
 
+},
+{
+
+},
+{
+
+}
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
