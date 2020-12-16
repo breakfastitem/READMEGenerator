b575acdb6adbe7c90e7aad6230d5e6d7964c1e58
breakfastitem <andrew.ehrman@gmail.com>
2020-12-16 03:35:12 -0500

    Added formatting content to generated markdown.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Table of Contents
+  [Description](#description)
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [License](#license)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## testing
+  ${data.testing}
+
+  ## contributions
+  ${data.guidelines}
+
+  ## license
+  ${data.license}
+
+  ## Questions
+  Github Repository: https://github.com/${data.username}
+
+  Emails regarding project should be sent to:
+
+  ${data.email}
+
 
 `;
 }
